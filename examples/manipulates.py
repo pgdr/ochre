ed71519c48c4dae11897d1d5d91835549d040137
Pål Grønås Drange <PGDR@statoil.com>
2019-03-24 14:16:10 +0100

    Add example for shearing

--- examples/manipulates.py
+++ examples/manipulates.py
@@ -6,18 +6,37 @@ import matplotlib.pyplot as plt
 import ochre
 
 
-def main(sent):
-    shi = 7
+def show_shift(sent):
+    shi = 2  # number of shifts each direction
     img = ochre.get_sentence(sent)
     for i in range(shi * 2):
-        img_ = ochre.generators.shift(ochre.get_sentence(sent), i - shi, 0)
+        img_ = ochre.generators.shift(ochre.get_sentence(sent), 2 * (i - shi), 0)
         img = np.concatenate((img, img_), axis=1)
     for i in range(shi * 2):
-        img_ = ochre.generators.shift(ochre.get_sentence(sent), 0, i - shi)
+        img_ = ochre.generators.shift(ochre.get_sentence(sent), 0, 2 * (i - shi))
         img = np.concatenate((img, img_), axis=1)
 
     plt.imshow(img)
     plt.show()
+
+
+def show_shear(sent):
+    she = 7
+    img = ochre.get_sentence(sent)
+
+    img_ = ochre.generators.shear(ochre.get_sentence(sent), shearing=she)
+    img = np.concatenate((img, img_), axis=1)
+
+    img_ = ochre.generators.shear(ochre.get_sentence(sent), shearing=-she)
+    img = np.concatenate((img, img_), axis=1)
+
+    plt.imshow(img)
+    plt.show()
+
+
+def main(sent):
+    show_shear(sent)
+    show_shift(sent)
 
 
 if __name__ == "__main__":
